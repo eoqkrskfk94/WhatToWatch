a091171ca8c8aadd7652f673d1473b2d0d99b81a
김명진 <mobinity.kmj@gmail.com>
2021-02-11 15:37:30 +0900

    [Add] 4번째 화면 추가 (OTT 서비스 선택 화면)

--- app/src/main/java/com/mobinity/whattowatch/view/FourthActivity.kt
+++ app/src/main/java/com/mobinity/whattowatch/view/FourthActivity.kt
@@ -2,11 +2,42 @@ package com.mobinity.whattowatch.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.databinding.DataBindingUtil
 import com.mobinity.whattowatch.R
+import com.mobinity.whattowatch.databinding.ActivityFourthBinding
+import com.mobinity.whattowatch.databinding.ActivityThirdBinding
+import com.mobinity.whattowatch.viewModel.FourthViewModel
 
 class FourthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fourth)
+        val binding: ActivityFourthBinding = DataBindingUtil.setContentView(this, R.layout.activity_fourth)
+        binding.lifecycleOwner = this
+        binding.viewModel = FourthViewModel()
+
+
+        setTextAnimation(binding)
+    }
+
+    private fun setTextAnimation(binding: ActivityFourthBinding){
+        val fadeIn = AnimationUtils.loadAnimation(applicationContext, R.anim.fadein)
+        val fadeIn2 = AnimationUtils.loadAnimation(applicationContext, R.anim.fadein)
+        fadeIn.duration = 2000
+        fadeIn2.duration = 2000
+
+        binding.tvCurrentQuestion.startAnimation(fadeIn)
+        binding.tvQuestion4.startAnimation(fadeIn)
+
+        binding.tvQuestion41.startAnimation(fadeIn2)
+        binding.tvQuestion42.startAnimation(fadeIn2)
+        binding.tvQuestion43.startAnimation(fadeIn2)
+        binding.tvQuestion44.startAnimation(fadeIn2)
+    }
+
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.fix, R.anim.slide_out_right)
     }
 }
---
+++ app/src/main/java/com/mobinity/whattowatch/viewModel/FourthViewModel.kt
@@ -0,0 +1,49 @@
+package com.mobinity.whattowatch.viewModel
+
+import android.app.Activity
+import android.content.Intent
+import android.view.View
+import com.mobinity.whattowatch.MyApplication
+import com.mobinity.whattowatch.R
+import com.mobinity.whattowatch.view.FifthActivity
+
+class FourthViewModel {
+
+    fun selectQuestion4(answer: String, view: View) {
+
+
+        when (answer) {
+
+            view.context.getString(R.string.dont_care) -> {
+                MyApplication.answer4 = view.context.getString(R.string.dont_care)
+            }
+
+            view.context.getString(R.string.netflix) -> {
+                MyApplication.answer4 = view.context.getString(R.string.netflix)
+            }
+
+            view.context.getString(R.string.watcha) -> {
+                MyApplication.answer4 = view.context.getString(R.string.watcha)
+            }
+
+            view.context.getString(R.string.wavve) -> {
+                MyApplication.answer4 = view.context.getString(R.string.wavve)
+            }
+
+
+        }
+
+        var intent = Intent(view.context, FifthActivity::class.java)
+        view.context.startActivity(intent)
+        (view.context as Activity).overridePendingTransition(R.anim.slide_in_right, R.anim.fix)
+
+    }
+
+    fun goBackBtn(view: View){
+
+        (view.context as Activity).finish()
+        (view.context as Activity).overridePendingTransition(R.anim.fix, R.anim.slide_out_right)
+
+    }
+
+}
